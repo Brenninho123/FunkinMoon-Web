3025e58ebcab625bb15f72ff712cbf4e1ea6bb57
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-09-07 19:42:30 -0300

    Add Version class with build, timestamp, and hash

---
+++ source/funkin/Version.js
@@ -0,0 +1,17 @@
+class Version {
+    static get build() {
+        return '2026.09.07-7327';
+    }
+
+    static get timestamp() {
+        return 1788820927000;
+    }
+
+    static get hash() {
+        return 'a7f3e9c1d8b2e4f6a5c3b1d9e2f4a6c8';
+    }
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Version;
+}
